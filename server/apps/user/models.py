36f28626e95d6787e94fff514566527920b15dc1
Sam-Radnus <samradnus2001@gmail.com>
2023-04-08 16:36:50 +0530

    Image and Bug Fix

--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -17,3 +17,4 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+
